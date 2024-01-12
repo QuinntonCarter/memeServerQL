1a46d4308c5b51d9929d6d023583edaa455225b8
QuinntonCarter <qdevsz@gmail.com>
2024-01-12 02:48:20 -0700

    even more scaled back server...

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 require("dotenv").config();
 // const { createHandler } = require("graphql-http/lib/use/express");
 // const colors = require("colors");
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
 
 // const schema = require("./schema/schema.js");
 // const connectDB = require("./config/db.js");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 // const developmentEnv =
 //   process.env.NODE_ENV === "dev" && "http://localhost:3000/";
 
