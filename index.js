df56bdebc2f3e1d744ef0653aead479c47148c62
QuinntonCarter <qdevsz@gmail.com>
2024-01-12 02:31:22 -0700

    Initial commit for testing

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const express = require("express");
 require("dotenv").config();
-const { createHandler } = require("graphql-http/lib/use/express");
-const colors = require("colors");
+// const { createHandler } = require("graphql-http/lib/use/express");
+// const colors = require("colors");
 const cors = require("cors");
 
 const app = express();
 
-const schema = require("./schema/schema.js");
+// const schema = require("./schema/schema.js");
 const connectDB = require("./config/db.js");
 const port = process.env.PORT || 8080;
-const developmentEnv =
-  process.env.NODE_ENV === "dev" && "http://localhost:3000";
+// const developmentEnv =
+//   process.env.NODE_ENV === "dev" && "http://localhost:3000/";
 
-const origin = [
-  process.env.CLIENT_URL,
-  process.env.CLIENT_URL_WEB,
-  "http://localhost:3000",
-];
+// const origin = [
+//   process.env.CLIENT_URL,
+//   process.env.CLIENT_URL_WEB,
+//   "http://localhost:3000/",
+// ];
 
-const corsOptions = {
-  origin: origin,
-};
-app.use(
-  cors({
-    origin: [
-      process.env.CLIENT_URL,
-      process.env.CLIENT_URL_WEB,
-      "http://localhost:3000",
-    ],
-  })
-);
+// const corsOptions = {
+//   origin: origin,
+// };
+// app.use(
+//   cors({
+//     origin: [
+//       process.env.CLIENT_URL,
+//       process.env.CLIENT_URL_WEB,
+//       "http://localhost:3000",
+//     ],
+//   })
+// );
+app.use(cors());
 app.use(express.json());
-
 // ignore favicon.ico request
-app.get("/favicon.ico", (req, res, next) => {
-  res.status(204);
-  next();
-});
+// app.get("/favicon.ico", (req, res, next) => {
+//   res.status(204);
+//   next();
+// });
 
 // connectDB();
 app.get("/", (req, res, next) => {
@@ -44,10 +44,10 @@ app.get("/", (req, res, next) => {
   next();
 });
 
-app.all("/graphql", createHandler({ schema }));
+// app.all("/graphql", createHandler({ schema }));
 
-connectDB().then(() => {
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+// connectDB().then(() => {
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+// });
